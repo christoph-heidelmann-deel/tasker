8c32fac7da239839f9178228ca3d551d6d7de3f9
de_loc_christoph@apple.com <deqe@Chris-MacBook-Pro.local>
2019-09-17 16:32:23 +0200

    Adds localStorage support

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,20 @@
 import { createStore } from "redux";
 import tasksReducer from "../reducers/index";
-const store = createStore(tasksReducer,{});
+import { login } from "../actions";
+
+let initialStoreData = {}
+if(localStorage.getItem("TaskerInitialStoreData")) {
+    initialStoreData = JSON.parse(localStorage.getItem("TaskerInitialStoreData"));
+}
+const store = createStore(tasksReducer, initialStoreData);
+
+window.addEventListener('storage', function(e) {  
+    let newStorageState = JSON.parse(localStorage.getItem("TaskerInitialStoreData"));
+    store.dispatch(login(newStorageState.user.username, newStorageState.user.tasks));
+});
+
+store.subscribe(() => {
+    localStorage.setItem("TaskerInitialStoreData", JSON.stringify(store.getState()));
+})
+
 export default store;
